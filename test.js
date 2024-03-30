bf8d440ce35ed1e8b0dbaa0e307a98955926dd73
FierdCS <93808907+FierdCS@users.noreply.github.com>
2024-03-30 18:48:00 +0000

    kens code

--- test.js
+++ test.js
@@ -1,45 +1,209 @@
-var fretCount;
-var tuning;
-/*
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-
-            openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-*/
-
-  window.onload = function(){
+var capo = 0;
+var fretCount = 22;
+var tuning = "Standard";
+let bestNote = [0,2]
+let E = [];
+let A = [];
+let D = [];
+let G = [];
+let B = [];
+let e = [];
+let tunings = ["Standard", "Eb", "DropC", "Dstand"] 
+let Standard = [164.81, 220, 293.66, 392, 493.88, 659.29]; //EADGBE
+let Eb = [155.56, 207.65, 277.18, 369.99, 466.16, 622.25]; //EbAbDbGbBbEb
+let DropC = [130.81, 196, 261.63, 349.23, 440, 589.33]; //CGCFAD
+let DStand = [146.83, 196, 261.63, 349.23, 440, 589.33]; //DGCFAD
+let DropD = [146.83, 220, 293.66, 392, 493.88, 659.29];//DADGBE
+window.onload = function(){ //inputs globals
     document.getElementById("fretSubmit").onclick = function(){
-    fretCount = parseInt(document.getElementById("fretct").value, 10);
+    fretCount = parseInt(document.getElementById("fretct").value, 10);//set fretcount
     console.log(fretCount);
-  }
+    setup();
+    }//change if requested
+
+    document.getElementById("capoSubmit").onclick = function(){
+    capo = parseInt(document.getElementById("capo").value, 10);//set where capo is
+    console.log(capo);
+    setup();//change if requested
+    }
+
+
 
     var dropdowns = document.getElementsByClassName("dropdown");
 
-    for (var dropdown of dropdowns) {
+    for (let dropdown of dropdowns) {
       let header = dropdown.getElementsByClassName("dropbtn")[0];
       let dropdownItems = dropdown.getElementsByClassName("dropdown-content")[0].children;
 
-      for (const dropdownItem of dropdownItems) {
-        dropdownItem.onClick = function() {
+      for (let dropdownItem of dropdownItems) {
+        dropdownItem.onclick = function() {
           if (header.classList.contains("show")) {
             header.classList.remove("show");
           }
 
           console.log(dropdownItem.textContent);
-          tuning = dropdownItem.textContent;
+          tuning = dropdownItem.textContent;//get tuning from user if unspecified assume standard
+          setup();//change if requested
         }
       }
     }
   }
+
+function setup()
+{
+  console.log("print"); //get correct tuning array
+  var tuneArr;
+  if(tuning === "Standard")
+    tuneArr = Standard;
+  else if(tuning === "Eb")
+    tuneArr = Eb;
+  else if(tuning === "Drop C")
+    tuneArr = DropD;
+  else if(tuning === "D Standard")
+    tuneArr = DStand;
+  else if(tuning === "Drop D")
+    tuneArr = DropD;
+  for(let i = 0; i < fretCount; i++)  //set up all notes on guitar
+  {
+    E[i] = tuneArr[0] * Math.pow(2,(i/12));
+    A[i] = tuneArr[1] * Math.pow(2,(i/12));
+    D[i] = tuneArr[2] * Math.pow(2,(i/12));
+    G[i] = tuneArr[3] * Math.pow(2,(i/12));
+    B[i] = tuneArr[4] * Math.pow(2,(i/12));
+    e[i] = tuneArr[5] * Math.pow(2,(i/12));
+  }
+  for(let b = 0; b < fretCount; b++)
+    console.log(D[b]);
+  
+  getNotes(415.3);
+}
+
+function getNotes(freq)
+{
+  let Cordinates = [];
+  if(freq >= E[capo] && freq <= E[fretCount - 1]) //check if note is in frequency of string
+  {
+    console.log("E start");
+    Cordinates.push(binary(E, capo, fretCount - 1, freq));//binary search to find respective note
+    Cordinates.push(0); 
+    console.log("E stop");
+  }
+  if(freq >= A[capo] && freq <= A[fretCount - 1])
+  {
+    Cordinates.push(binary(A, capo, fretCount -1, freq));
+    Cordinates.push(1);
+  }
+  if(freq >= D[capo] && freq <= D[fretCount - 1])
+  {
+    Cordinates.push(binary(D, capo, fretCount -1, freq));
+    Cordinates.push(2);
+  }
+  if(freq >= G[capo] && freq <= G[fretCount - 1])
+  {
+    Cordinates.push(binary(G, capo, fretCount -1, freq));
+    Cordinates.push(3);
+  }
+  if(freq >= B[capo] && freq <= B[fretCount - 1])
+  {
+    Cordinates.push(binary(B, capo, fretCount -1, freq));
+    Cordinates.push(4);
+  }
+  if(freq >= e[capo] && freq <= e[fretCount - 1])
+  {
+    Cordinates.push(binary(e, capo, fretCount -1, freq));
+    Cordinates.push(5);
+  }
+  console.log(Cordinates)
+  bestChoice(Cordinates);
+}
+
+function bestChoice(arr)
+{
+  let x = arr.filter((element,index) => {return index % 2 === 0;
+  })
+  console.log(x);
+  if(bestNote.length == 0) //if we do not have previous note set note to one closest to open string
+  {
+    var lowest = 0;
+    for(var i = 1; i < x.length; i++)
+    {
+      if(x[i] < x[lowest]) lowest = i;
+    }
+    bestNote.push(x[lowest]);
+    bestNote.push(lowest);
+    console.log(bestNote);
+  }
+  else
+  {
+    let dist = [] 
+    for(var i = 0; i < x.length; i++)//if we have previous note
+    {
+      dist.push(Math.sqrt(Math.pow((x[i] - bestNote[0]),2) + Math.pow((i-bestNote[1]),2))); //use distance formula (value is x index is y) to find closes note
+    }
+    lowest = 0;
+    for(var i = 1; i < dist.length; i++)
+    {
+      if(dist[i] < dist[lowest]) lowest = i; //find smallest element from distance formula
+    }
+    console.log("before" + bestNote);
+    bestNote[0] = x[lowest];
+    bestNote[1] = lowest;
+    console.log("after" + bestNote);
+  }
+  
+}
+
+function binary(arr, l, r, x)
+{
+  while (l < r) {
+      let m =  l + (r - l) / 2;
+      m = Math.floor(m);
+      // Check if x is present at mid
+      console.log("freq" + x);
+      console.log("array val " +  arr[m] + "checkval" + m);
+      if (arr[m] == x)
+          return m;
+
+      // If x greater, ignore left half
+      if (arr[m] < x)
+          l = m + 1;
+
+      // If x is smaller, ignore right half
+      else
+          r = m - 1;
+  }
+
+  // If we reach here, then element was
+  // not present
+  if(l - 1 - capo < 0)
+  {
+    if(Math.abs(arr[l + 1] -x) > Math.abs(arr[l] - x))
+      return l - capo;
+    return l + 1 - capo;
+  }
+
+  if(l + 1 - capo >= arr.length)
+  {
+    if(Math.abs(arr[l - 1] -x) > Math.abs(arr[l] - x))
+      return l - capo;
+    return l - 1 - capo;
+  }
+
+  //console.log("l+1: " + arr[l+1] + "l" + arr[l] + "l-1" + arr[l-1]);
+  if(Math.abs(arr[l - 1] - x) > Math.abs(arr[l] - x) && Math.abs(arr[l + 1] - x) > Math.abs(arr[l] - x))
+  {
+    //console.log("return l: " + arr[l] + "index: " + l);
+    return l - capo;
+  }
+  else if(Math.abs(arr[l - 1] - x) > Math.abs(arr[l + 1] - x) && Math.abs(arr[l] - x) > Math.abs(arr[l + 1] - x))
+  {
+  //console.log("return l-1: " + arr[l-1] + "index: " + l+1);
+    return l + 1 - capo;
+  }
+  else
+    return l - 1 - capo;
+  
+}
 
   let mediaRecorder;
   let recordedChunks = [];
